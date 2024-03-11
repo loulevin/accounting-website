79ed8df84a2188a13eefca831725ca618a012479
loulevin <patricia.balbier@gmail.com>
2024-03-11 09:14:08 +0100

    nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 export const Nav = () => {
   return (
-    <nav className="ml-5 w-[8rem]">
+    <nav className="ml-5 w-[8rem] mt-3">
       <NavLink to="/" >Login</NavLink>
     </nav>
   );
